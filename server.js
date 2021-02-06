35ae7273eb47884a90c3164a2548df798e6278ee
Elmer C. Galo <elmer.galo@outlook.com>
2021-02-06 02:28:29 -0500

    Initialize Mongoose and create Auth key

--- server.js
+++ server.js
@@ -12,6 +12,23 @@ app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const db = require("./app/models");
+const Role = db.role;
+
+db.mongoose
+  .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Successfully connect to MongoDB");
+    initial();
+  })
+  .catch((err) => {
+    console.error("Connection error ", err);
+    process.exit();
+  });
+
 // Simple route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to this JWT application" });
@@ -21,3 +38,39 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+function initial() {
+  Role.estimatedDocumentCount((err, count) => {
+    if (!err && count === 0) {
+      new Role({
+        name: "user",
+      }).save((err) => {
+        if (err) {
+          console.log("Error ", err);
+        }
+
+        console.log("Added 'user' to roles collection");
+      });
+
+      new Role({
+        name: "moderator",
+      }).save((err) => {
+        if (err) {
+          console.log("Error ", err);
+        }
+
+        console.log("Added 'moderator' to roles collection");
+      });
+
+      new Role({
+        name: "admin",
+      }).save((err) => {
+        if (err) {
+          console.log("Error ", err);
+        }
+
+        console.log("Added 'admin' to roles collection");
+      });
+    }
+  });
+}
